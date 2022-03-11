1464decb8793d1fde21454d4b6b73de91a96707a
Jmartinez03101 <j.martinez2@ardencahillacademy.com>
2022-03-11 16:52:57 +0000

    to do 24

--- projects/catstagram/javascript/brains.js
+++ projects/catstagram/javascript/brains.js
@@ -1,1 +1,12 @@
-
+// let likeCounter = counter1;
+var counter1=0;
+likeCount=[counter1,counter2,counter3,counter4,counter5];
+// function likePhoto(){
+// counter1 += 1;
+// console.log(counter1);
+// document.getElementById("likeCount1").innerHTML = counter1;
+// }
+$("#button2").on("click", { buttonNumber : 2 }, increaseLikes)
+function increaseLikes(event) {
+   // event.data.buttonNumber would have access to the value 2 here
+   }
