7c1d29c808e4aaff026fb5a28ec6f8b0b71dc9e8
Jmartinez03101 <j.martinez2@ardencahillacademy.com>
2022-03-14 14:59:11 +0000

    to do 24 and css for profile

--- projects/catstagram/javascript/brains.js
+++ projects/catstagram/javascript/brains.js
@@ -1,11 +1,23 @@
 // let likeCounter = counter1;
 var counter1=0;
-likeCount=[counter1,counter2,counter3,counter4,counter5];
-// function likePhoto(){
-// counter1 += 1;
-// console.log(counter1);
-// document.getElementById("likeCount1").innerHTML = counter1;
-// }
+likeCount=[0,0,0,0,0];
+function likePhoto(num){
+likeCount[num]++;
+console.log(likeCount);
+document.getElementById("likeCount" + num).innerHTML = likeCount[num];
+}
+var user = {
+'name':'Julian',
+'name_last':'Martinez',
+'followers':['Sam','Tim','Antoine','Louis','Jeremiah'],
+'profile_pic':'http://pds.exblog.jp/pds/1/201003/22/90/a0126590_0331777.jpg',
+'bio':'Cat Doge go dooooge'
+}
+setTimeout(function(){document.getElementById('bio').innerHTML = user.bio;}, 3000);
+setTimeout(function(){ document.getElementById("name").innerHTML = user.name;}, 3000);
+setTimeout(function(){ document.getElementById("name_last").innerHTML = user.name_last;}, 3000);
+setTimeout(function(){ document.getElementById("followers").innerHTML = "Followers: " + user.followers.length;}, 3000);
+setTimeout(function(){ $("#profile_pic").attr("src", user.profile_pic);}, 3000);
 $("#button2").on("click", { buttonNumber : 2 }, increaseLikes)
 function increaseLikes(event) {
    // event.data.buttonNumber would have access to the value 2 here
