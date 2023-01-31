401b4362e29f0db3a846b91da4d9d7c9978f9702
Jmartinez03101 <j.martinez2@ardencahillacademy.com>
2023-01-31 15:43:39 +0000

    added new project

--- Personal website/index.js
+++ Personal website/index.js
@@ -1,6 +1,11 @@
 // var dinoLink = 'https://offline-dino-game.firebaseapp.com/';
-$('#startBtn').on('click', swap);
-// $('.class').append('<a href="#" onclick="myFunction(\''+link+'\')">
-function swap(){
-    $('#startbtn').remove();
-}
+$(document).ready(function () {
+    debugger;
+    $('#startBtn').click(swap() {
+        $(this).swap();
+    });
+    // $('.class').append('<a href="#" onclick="myFunction(\''+link+'\')">
+    $.fn.swap = swap() {
+        console.log('lllll');
+    }
+});
