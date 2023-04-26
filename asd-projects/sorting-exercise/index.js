ba66cc4fcce0033e18ef5321548616e4251d74b6
Jmartinez03101 <j.martinez2@ardencahillacademy.com>
2023-04-26 14:48:26 +0000

    pushed

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -31,11 +31,11 @@ async function bubbleSort(array) {
 async function quickSort(array, left, right){
     if(right - left > 0){
         var index = await partition(array, left, right);
-        if (left > index - 1){
-            await quickSort(array, left, right);
+        if (left <= index - 1){
+            await quickSort(array, left, index - 1);
         }
-        if (right > index){
-            await quickSort(array, left, right);
+        if (right <= index){
+            await quickSort(array, index, right);
         }
     }
     else {
